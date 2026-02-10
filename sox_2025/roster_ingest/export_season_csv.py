fa2abf6123e1280521e0cb66a27ad3215f50157c
ctaylr13 <ctaylr13@gmail.com>
2026-02-10 14:16:39 -0500

    updated file name

--- sox_2025/roster_ingest/export_season_csv.py
+++
@@ -1,7 +0,0 @@
-import duckdb
-import pandas as pd
-
-conn = duckdb.connect("redsox_25.duckdb")
-df = conn.execute('SELECT * FROM "2025_schedule" ORDER BY gameDate').df()
-conn.close()
-df.to_csv("2025_schedule.csv", index=False)
