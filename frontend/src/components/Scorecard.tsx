9cf7de77f0aeb843d29b329a1a7bba85f1154f2a
ctaylr13 <ctaylr13@gmail.com>
2026-02-14 20:30:53 -0500

    initial page set up

---
+++ frontend/src/components/Scorecard.tsx
@@ -0,0 +1,12 @@
+const Scorecard = () => {
+    return (
+        <div>
+            Scorecard
+            <div>Top</div>
+            <div>Middle</div>
+            <div>Bottom</div>
+        </div>
+    );
+};
+
+export default Scorecard;
