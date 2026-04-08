fb1123ee2d24e5e8ad0a86a4f3e1a5d6183063f8
ctaylr13 <ctaylr13@gmail.com>
2026-04-08 11:28:16 -0400

    updated scorecard

--- frontend/src/components/Scorecard/Scorecard.tsx
+++ frontend/src/components/Scorecard/Scorecard.tsx
@@ -1,51 +1,50 @@
 import { styled } from "@linaria/react";
+import AtBat from "./AtBat";
 
-const StyledRow = styled.div`
+const ScoreGrid = styled.div`
+    display: grid;
+    grid-template-columns:
+        160px 60px
+        repeat(11, 40px)
+        40px 40px 40px 50px 40px;
+    gap: 0.5rem;
+    align-items: center;
+`;
+
+const RowStyled = styled.div`
     display: flex;
     flex-direction: row;
-    gap: 1rem;
-    padding-top: 1rem;
+    /* width: 100%; */
 `;
+
 const Scorecard = () => {
+    const innings = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
     return (
         <div>
-            <StyledRow>
-                <div>Player</div>
-                <div>Pos.</div>
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
-                <div>7</div>
-                <div>8</div>
-                <div>9</div>
-                <div>10</div>
-                <div>11</div>
-                <div>AB</div>
-                <div>R</div>
-                <div>H</div>
-                <div>RBI</div>
-                <div>SB</div>
-            </StyledRow>
-            <StyledRow>
-                <div>H/R/LOB/SB</div>
-                <div>1/2</div>
-                <div>1/-/2</div>
-                <div>-</div>
-                <div>-</div>
-                <div>1/2</div>
-                <div>1/-/2</div>
-                <div>-</div>
-                <div>-</div>
-                <div>-</div>
-                <div>45</div>
-                <div>4</div>
-                <div>6</div>
-                <div>4</div>
-                <div>3</div>
-            </StyledRow>
+            <div>Player</div>
+            <div>Pos.</div>
+            {innings.map((inning) => (
+                <div key={inning}>{inning}</div>
+            ))}
+            <div>AB</div>
+            <div>R</div>
+            <div>H</div>
+            <div>RBI</div>
+            <div>SB</div>
+            <div>Betts</div>
+            <div>RF</div>
+            <RowStyled>
+                {innings.map((inning) => (
+                    // <div key={`bett-${inning}`}>
+                    <AtBat key={`bett-${inning}`} />
+                    // </div>
+                ))}
+            </RowStyled>
+            <div>4</div>
+            <div>1</div>
+            <div>2</div>
+            <div>1</div>
+            <div>0</div>
         </div>
     );
 };
